5081898fef1105b3685d1f03e1cbb17a5fda387f
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-03-02 22:32:20 +0900

    add JSONDecoder++

---
+++ Sources/TripadvisorKit/JSONDecoder++.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+extension JSONDecoder {
+  static var tripadvisor: JSONDecoder {
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .custom { decoder in
+      let formatter = ISO8601DateFormatter()
+      let string = try decoder.singleValueContainer().decode(String.self)
+      if let date = formatter.date(from: string) {
+        return date
+      } else {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter.date(from: string)!
+      }
+    }
+    return decoder
+  }
+}
