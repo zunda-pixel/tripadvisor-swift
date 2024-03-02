9a3ab6033a2161400d9c583cbd6f34038143c070
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-03-02 17:09:48 +0900

    add Test

--- Tests/TripadvisorKitTests/TripadvisorKitTests.swift
+++ Tests/TripadvisorKitTests/TripadvisorKitTests.swift
@@ -1,5 +1,26 @@
 import XCTest
+
 @testable import TripadvisorKit
 
 final class TripadvisorKitTests: XCTestCase {
+  let api = TripadvisorAPI(
+    apiKey: ProcessInfo.processInfo.environment["API_KEY"]!,
+    language: .ja,
+    referer: URL(string: ProcessInfo.processInfo.environment["REFERER_URL"]!)!
+  )
+
+  func testSearchLocation() async throws {
+    let query = "海老丸らーめん"
+    let locations = try await api.searchLocation(query: query)
+    let location = locations.first!
+    XCTAssertEqual(location.id, "15654717")
+    XCTAssertEqual(location.name, "海老丸 らーめん")
+    XCTAssertEqual(location.address.street1, "西神田2-1-13")
+    XCTAssertEqual(location.address.street2, "十勝ビル1F")
+    XCTAssertEqual(location.address.city, "千代田区")
+    XCTAssertEqual(location.address.state, "東京都")
+    XCTAssertEqual(location.address.country, "日本")
+    XCTAssertEqual(location.address.postalCode, "〒101-0065")
+    XCTAssertEqual(location.address.addressString, "〒101-0065 東京都 千代田区 西神田2-1-13 十勝ビル1F")
+  }
 }
