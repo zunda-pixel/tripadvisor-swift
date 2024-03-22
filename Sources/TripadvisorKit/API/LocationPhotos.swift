4d8b67614d1aafeaeb0e8624b7a3cc238c71d32a
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-03-23 03:10:03 +0900

    add Document

--- Sources/TripadvisorKit/API/LocationPhotos.swift
+++ Sources/TripadvisorKit/API/LocationPhotos.swift
@@ -1,6 +1,14 @@
 import Foundation
 
 extension TripadvisorAPI {
+  /// The Location Photos request returns up to 5 high-quality photos for a specific location. Please note that the limits are different for the beta subscribers. You need to upgrade to get the higher limits mentioned here.
+  /// https://tripadvisor-content-api.readme.io/reference/getlocationphotos
+  /// - Parameters:
+  ///   - locationId: A unique identifier for a location on Tripadvisor. The location ID can be obtained using the Location Search.
+  ///   - limit: The number of results to return
+  ///   - offset: The index of the first result
+  ///   - source: A comma-separated list of allowed photo sources.
+  /// - Returns: [``Photo``]
   public func locationPhotos(
     id locationId: Location.ID,
     limit: Int? = nil,
