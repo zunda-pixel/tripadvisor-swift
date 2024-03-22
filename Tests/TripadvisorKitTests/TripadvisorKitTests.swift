557d9935a5bec89f29a9ae1356416dce83d1f094
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-03-23 02:58:21 +0900

    fix test

--- Tests/TripadvisorKitTests/TripadvisorKitTests.swift
+++ Tests/TripadvisorKitTests/TripadvisorKitTests.swift
@@ -13,7 +13,7 @@ final class TripadvisorKitTests: XCTestCase {
     let query = "Apple Park Visitor Center"
     let locations = try await api.searchLocations(query: query)
     let location = locations.first!
-    XCTAssertEqual(location.id, 13331376)
+    XCTAssertEqual(location.id, 13_331_376)
     XCTAssertEqual(location.name, "Apple Park Visitor Center")
     XCTAssertEqual(location.address?.street1, "10600 N Tantau Ave")
     XCTAssertNil(location.address?.street2)
@@ -28,7 +28,7 @@ final class TripadvisorKitTests: XCTestCase {
     let point = Location.Point(latitude: 37.334603, longitude: -122.009198)
     let locations = try await api.searchNearLocations(point: point)
     let location = locations.first!
-    XCTAssertEqual(location.id, 13331376)
+    XCTAssertEqual(location.id, 13_331_376)
     XCTAssertEqual(location.name, "Apple Park Visitor Center")
     XCTAssertEqual(location.address?.street1, "10600 N Tantau Ave")
     XCTAssertNil(location.address?.street2)
@@ -40,19 +40,19 @@ final class TripadvisorKitTests: XCTestCase {
   }
 
   func testLocationReviews() async throws {
-    let locationId: Location.ID = 13331376
+    let locationId: Location.ID = 13_331_376
     let reviews = try await api.locationReviews(id: locationId)
-    XCTAssertTrue(reviews.map(\.locationId).allSatisfy { $0 == locationId.rawValue })
+    XCTAssertTrue(reviews.map(\.locationId).allSatisfy { $0 == locationId })
   }
 
   func testLocationPhotos() async throws {
-    let locationId: Location.ID = 13331376
+    let locationId: Location.ID = 13_331_376
     let photos = try await api.locationPhotos(id: locationId)
     print(photos)
   }
 
   func testLocatioDetail() async throws {
-    let locationId: Location.ID = 13331376
+    let locationId: Location.ID = 13_331_376
     let location = try await api.locationDetail(id: locationId)
     print(location)
     XCTAssertEqual(location.id, locationId)
@@ -64,41 +64,78 @@ final class TripadvisorKitTests: XCTestCase {
     XCTAssertEqual(location.address?.country, "United States")
     XCTAssertEqual(location.address?.postalCode, "95014-0739")
     XCTAssertEqual(location.address?.addressString, "10600 N Tantau Ave, Cupertino, CA 95014-0739")
-    XCTAssertEqual(location.description, "Step into the heart of the Apple empire at this sleek and modern building—the only part of the Apple Park campus that’s open to the public. Here, you can tour the innovative exhibition space for a behind-the-scenes look at Apple’s journey, technology, and design principles, head up to the roof terrace for an expansive view of Apple Park, and rub shoulders with other creatives at the cafe. Of course, the highlight of the center is the massive Apple Store where you can browse their full range of products and snag exclusive merchandise. You can check out the center and other tech giants on a tour of Silicon Valley. – Tripadvisor")
-    XCTAssertEqual(location.webURL, URL(string: "https://www.tripadvisor.com/Attraction_Review-g32273-d13331376-Reviews-Apple_Park_Visitor_Center-Cupertino_California.html?m=66827")!)
-    XCTAssertEqual(location.ancestors, [
-      .init(level: "City", name: "Cupertino", locationId: "32273"),
-      .init(level: "State", name: "California", locationId: "28926"),
-      .init(level: "Country", name: "United States", locationId: "191")
-    ])
+    XCTAssertEqual(
+      location.description,
+      "Step into the heart of the Apple empire at this sleek and modern building—the only part of the Apple Park campus that’s open to the public. Here, you can tour the innovative exhibition space for a behind-the-scenes look at Apple’s journey, technology, and design principles, head up to the roof terrace for an expansive view of Apple Park, and rub shoulders with other creatives at the cafe. Of course, the highlight of the center is the massive Apple Store where you can browse their full range of products and snag exclusive merchandise. You can check out the center and other tech giants on a tour of Silicon Valley. – Tripadvisor"
+    )
+    XCTAssertEqual(
+      location.webURL,
+      URL(
+        string:
+          "https://www.tripadvisor.com/Attraction_Review-g32273-d13331376-Reviews-Apple_Park_Visitor_Center-Cupertino_California.html?m=66827"
+      )!)
+    XCTAssertEqual(
+      location.ancestors,
+      [
+        .init(level: "City", name: "Cupertino", locationId: 32273),
+        .init(level: "State", name: "California", locationId: 28926),
+        .init(level: "Country", name: "United States", locationId: 191),
+      ])
     XCTAssertEqual(location.latitude, 37.33272)
     XCTAssertEqual(location.longitude, -122.00533)
     XCTAssertEqual(location.timezone, "America/Los_Angeles")
     XCTAssertEqual(location.phoneNumber, "+1 408-961-1560")
-    XCTAssertEqual(location.website, URL(string: "http://www.apple.com/retail/appleparkvisitorcenter/")!)
-    XCTAssertEqual(location.writeReview, URL(string: "https://www.tripadvisor.com/UserReview-g32273-d13331376-Apple_Park_Visitor_Center-Cupertino_California.html?m=66827")!)
-    XCTAssertEqual(location.ranking, .init(geoLocationId: "32273", rankingString: "#3 of 30 things to do in Cupertino", geoLocationName: "Cupertino", rankingOutOf: "30", ranking: "3"))
-    XCTAssertEqual(location.rating, "4.0")
-    XCTAssertEqual(location.ratingImageUrl, URL(string: "https://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/4.0-66827-5.svg")!)
-    XCTAssertEqual(location.reviewRatingCount, ["2": "13", "5": "107", "4": "59", "3": "52", "1": "8"])
-    XCTAssertEqual(location.photoCount, "424")
-    XCTAssertEqual(location.seeAllPhotosURL, URL(string: "https://www.tripadvisor.com/Attraction_Review-g32273-d13331376-m66827-Reviews-Apple_Park_Visitor_Center-Cupertino_California.html#photos")!)
+    XCTAssertEqual(
+      location.website, URL(string: "http://www.apple.com/retail/appleparkvisitorcenter/")!)
+    XCTAssertEqual(
+      location.writeReview,
+      URL(
+        string:
+          "https://www.tripadvisor.com/UserReview-g32273-d13331376-Apple_Park_Visitor_Center-Cupertino_California.html?m=66827"
+      )!)
+    XCTAssertEqual(
+      location.ranking,
+      .init(
+        geoLocationId: 32273,
+        rankingString: "#3 of 30 things to do in Cupertino",
+        geoLocationName: "Cupertino",
+        rankingOutOf: 30,
+        ranking: 3
+      )
+    )
+    XCTAssertEqual(location.rating, 4.0)
+    XCTAssertEqual(
+      location.ratingImageUrl,
+      URL(string: "https://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/4.0-66827-5.svg")!)
+    XCTAssertEqual(location.reviewRatingCount, [2: 13, 5: 107, 4: 59, 3: 52, 1: 8])
+    XCTAssertEqual(location.photoCount, 424)
+    XCTAssertEqual(
+      location.seeAllPhotosURL,
+      URL(
+        string:
+          "https://www.tripadvisor.com/Attraction_Review-g32273-d13331376-m66827-Reviews-Apple_Park_Visitor_Center-Cupertino_California.html#photos"
+      )!)
     XCTAssertEqual(location.priceLevel, nil)
     XCTAssertEqual(location.hours, nil)
     XCTAssertEqual(location.features, nil)
     XCTAssertEqual(location.cuisines, nil)
-    XCTAssertEqual(location.category, .init(name: "attraction", localizedName: "Attraction", value: nil))
-    XCTAssertEqual(location.subCategories, [
-      .init(name: "landmarks", localizedName: "Sights & Landmarks", value: nil),
-      .init(name: "attractions", localizedName: "Attractions", value: nil)
-    ])
+    XCTAssertEqual(
+      location.category, .init(name: "attraction", localizedName: "Attraction", value: nil))
+    XCTAssertEqual(
+      location.subCategories,
+      [
+        .init(name: "landmarks", localizedName: "Sights & Landmarks", value: nil),
+        .init(name: "attractions", localizedName: "Attractions", value: nil),
+      ])
     XCTAssertEqual(location.neighborhoods, [])
-    XCTAssertEqual(location.tripTypes, [
-      .init(name: "business", localizedName: "Business", value: "18"),
-      .init(name: "couples", localizedName: "Couples", value: "52"),
-      .init(name: "solo", localizedName: "Solo travel", value: "35"),
-      .init(name: "family", localizedName: "Family", value: "53"),
-      .init(name: "friends", localizedName: "Friends getaway", value: "30"),
-    ])
+    XCTAssertEqual(
+      location.tripTypes,
+      [
+        .init(name: "business", localizedName: "Business", value: "18"),
+        .init(name: "couples", localizedName: "Couples", value: "52"),
+        .init(name: "solo", localizedName: "Solo travel", value: "35"),
+        .init(name: "family", localizedName: "Family", value: "53"),
+        .init(name: "friends", localizedName: "Friends getaway", value: "30"),
+      ])
   }
 }
