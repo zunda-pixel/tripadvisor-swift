925d7bd3bd02f25fe5c3ac59d3999434241486da
zunda <zunda.dev@gmail.com>
2024-10-13 00:01:18 +0900

    update library

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-http-types", from: "1.3.0"),
     .package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.10.0"),
     .package(url: "https://github.com/zunda-pixel/swift-currency", from: "0.0.1"),
-    .package(url: "https://github.com/swiftlang/swift-testing", from: "0.11.0"),
-    .package(url: "https://github.com/zunda-pixel/http-client", from: "0.1.3"),
+    .package(url: "https://github.com/swiftlang/swift-testing", from: "0.99.0"),
+    .package(url: "https://github.com/zunda-pixel/http-client", from: "0.3.0"),
   ],
   targets: [
     .target(
